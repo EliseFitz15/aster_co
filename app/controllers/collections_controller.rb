051558163e965776ab3787bcbf22ad0ffa10ab9c
Elise Fitzgerald <elise.fitzgerald15@gmail.com>
2016-01-23 21:03:36 -0500

    Adds lists of products on collection page

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,7 +1,11 @@
 class CollectionsController < ApplicationController
-
+  def index
+    @collections = Collection.all
+  end
+  
   def show
     @collection = Collection.find(params[:id])
+    @products = @collection.products
   end
 
   def new
---
+++ spec/features/collections/sees_all_products_in_collection_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+feature 'sees collections list and products listed on collection page' do
+  let!(:collection) { create(:collection) }
+  let!(:collection_2) { create(:collection, title: "Burning man") }
+  let!(:product_1) { create(:product, collection: collection) }
+  let!(:product_2) { create(:product, collection: collection) }
+  let!(:product_3) { create(:product, collection: collection_2) }
+
+  scenario 'list of collections' do
+    visit collections_path
+
+    expect(page).to have_link collection.title
+    expect(page).to have_link collection_2.title
+  end
+
+  scenario 'list of products in collection' do
+    visit collection_path(collection)
+
+    expect(page).to have_content(product_1.name)
+    expect(page).to have_content(product_2.name)
+    expect(page).not_to have_content(product_3.name)
+
+    click_on "Collections"
+
+    click_on collection_2.title
+
+    expect(page).to have_content product_3.name
+  end
+end
