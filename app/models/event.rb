e0a243e6cb8df94f9ea9911314d72b081f35ccd8
Elise Fitzgerald <elise.fitzgerald15@gmail.com>
2016-03-05 16:15:25 -0500

    adds events form and page.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,10 +1,5 @@
 class Event < ActiveRecord::Base
   validates :title, presence: true
-  validates :details, presence: true
   validates :start_date, presence: true
   validates :location_name, presence: true
-  validates :address, presence: true
-  validates :zip_code, presence: true
-  validates :zip_code, length: { is: 5 }
-  validates :zip_code, numericality: { only_integer: true }
 end
