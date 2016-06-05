7eba78ad5199be3787b872a46532972ed452dc7d
Elise Fitzgerald <elise.fitzgerald15@gmail.com>
2016-06-05 13:36:04 -0400

    account for failed api call

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -13,9 +13,10 @@ class HomesController < ApplicationController
   def welcome
     @photos = []
     @data = HTTParty.get('https://api.instagram.com/v1/users/' + ENV['IG_USER'] +   '/media/recent/?access_token=' + ENV['ACCESS_TOKEN'] + '')
-
-    3.times do |i|
-      @photos << @data["data"][i]["images"]["low_resolution"]["url"]
+    unless @data["data"].nil?
+      3.times do |i|
+        @photos << @data["data"][i]["images"]["low_resolution"]["url"]
+      end
     end
   end
 
