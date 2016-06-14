07e02d75474dcf25e59123a76f9f4e30ac01a443
Elise Fitzgerald <elise.fitzgerald15@gmail.com>
2016-06-14 17:31:14 -0400

    Embeds instagram photos

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -11,13 +11,13 @@ class HomesController < ApplicationController
   end
 
   def welcome
-    @photos = []
-    @data = HTTParty.get('https://api.instagram.com/v1/users/' + ENV['IG_USER'] +   '/media/recent/?access_token=' + ENV['ACCESS_TOKEN'] + '')
-    unless @data["data"].nil?
-      3.times do |i|
-        @photos << @data["data"][i]["images"]["low_resolution"]["url"]
-      end
-    end
+    # @photos = []
+    # @data = HTTParty.get('https://api.instagram.com/v1/users/' + ENV['IG_USER'] +   '/media/recent/?access_token=' + ENV['ACCESS_TOKEN'] + '')
+    # unless @data["data"].nil?
+    #   3.times do |i|
+    #     @photos << @data["data"][i]["images"]["low_resolution"]["url"]
+    #   end
+    # end
   end
 
   def placeholdevents
